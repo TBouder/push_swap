1a13f0c8b52c110290b3be2841ab6213d20ab8a5
Tbouder <tbouder@protonmail.com>
2016-04-01 15:30:50 +0200

    Auto-Update 15:30:50, Friday 01 April

--- ft_printf/test_main.c
+++ ft_printf/test_main.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 18:34:22 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/01 15:00:11 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/01 15:11:45 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ int		main()
 	setlocale(LC_ALL, "en_US.UTF-8");
 
 	int	i;
-	int	j;
+	// int	j;
 
-	i = ft_printf("Bonjour, %C, %d\n", 57343, 42);
-	ft_printf("\n\n\n");
-	j = printf("Bonjour, %C, %d\n", 57343, 42);
-	printf("\n%d vs %d\n", i, j);
+	// ft_putchar(231);
+	// ft_putchar(177);
+	// ft_putchar(178);
+
+	i = ft_printf("Bonjour, %C, %d\n", L'米', 42);
+	// ft_printf("\n\n\n");
+	// j = printf("Bonjour, %C, %d\n", 57343, 42);
+	// printf("\n%d vs %d\n", i, j);
 
 
 	return (0);
