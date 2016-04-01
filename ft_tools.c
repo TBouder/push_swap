4a6c8059b935e915af33a3a72683ae1f67f9b5ba
Tbouder <tbouder@protonmail.com>
2016-04-01 16:06:11 +0200

    Auto-Update 16:06:11, Friday 01 April

--- ft_tools.c
+++ ft_tools.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 14:03:11 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/01 15:57:33 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/01 16:05:52 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,10 @@ int		ft_atoi_swap(const char *str)
 void	ft_check_min_man(long value)
 {
 	if ((int)value > 2147483647 || (int)value <= -2147483648)
+	{
+		ft_putendl("HI");
 		ft_error();
+	}
 }
 
 int		ft_is_sorted(t_swap *swap)
