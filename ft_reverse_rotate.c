52b0e4b5fa9a6350bd06110f8822f3fbdbd291e1
Tbouder <tbouder@protonmail.com>
2016-04-06 19:59:56 +0200

    Swap the two name files

--- ft_reverse_rotate.c
+++ ft_reverse_rotate.c
@@ -1,85 +1,84 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_reverse_rotate.c                                :+:      :+:    :+:   */
+/*   ft_rotate.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/29 23:57:54 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/05 16:21:33 by tbouder          ###   ########.fr       */
+/*   Created: 2016/03/29 22:54:39 by tbouder           #+#    #+#             */
+/*   Updated: 2016/04/06 16:32:22 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-void	ra(t_swap *a)
+void	rra(t_swap *a)
 {
 	int		first;
 	int		activ;
 
-	a = a->end;
-	while (a->activ == 0)
-		a = a->prev;
-	if (a)
+	first = 0;
+	activ = 0;
+	if (a && a->activ == 1)
 	{
 		first = a->stack;
 		activ = a->activ;
 	}
-	while (a->prev)
+	while (a->next)
 	{
-		a->stack = a->prev->stack;
-		a->activ = a->prev->activ;
-		a = a->prev;
+		a->stack = a->next->stack;
+		a->activ = a->next->activ;
+		a = a->next;
 	}
 	a->stack = first;
 	a->activ = activ;
 	ft_push_front(a->start);
 }
 
-void	rr(t_swap *a, t_swap *b)
+void	rrr(t_swap *a, t_swap *b)
 {
-	ra(a);
-	ra(b);
+	rra(a);
+	rra(b);
 }
 
-void	ft_rr_a(t_swap *a, t_swap *b, t_flag *flg)
+void	ft_rrr_a(t_swap *a, t_swap *b, t_flag *flg)
 {
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 	if (flg->color)
-		flg->verbose ? ft_printf("{c}ra{0}\n") : ft_printf("{c}ra{0} ");
+		flg->verbose ? ft_printf("{c}rra{0}\n") : ft_printf("{c}rra{0} ");
 	else
-		flg->verbose ? ft_printf("ra\n") : ft_printf("ra ");
-	ra(a);
+		flg->verbose ? ft_printf("rra\n") : ft_printf("rra ");
+	rra(a);
 	flg->verbose ? ft_flag_v(a, b) : 0;
 	flg->cnt += 1;
-	ft_calls_rev_rot(a, b, flg);
+	ft_calls_rot(a, b, flg);
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 }
 
-void	ft_rr_b(t_swap *a, t_swap *b, t_flag *flg)
+void	ft_rrr_b(t_swap *a, t_swap *b, t_flag *flg)
 {
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 	if (flg->color)
-		flg->verbose ? ft_printf("{b}rb{0}\n") : ft_printf("{b}rb{0}");
+		flg->verbose ? ft_printf("{b}rrb{0}\n") : ft_printf("{b}rrb{0} ");
 	else
-		flg->verbose ? ft_printf("rb\n") : ft_printf("rb");
-	ra(b);
+		flg->verbose ? ft_printf("rrb\n") : ft_printf("rrb ");
+	rra(b);
 	flg->verbose ? ft_flag_v(a, b) : 0;
 	flg->cnt += 1;
-	ft_calls_rev_rot(a, b, flg);
+	ft_calls_rot(a, b, flg);
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 }
 
-void	ft_rr_ab(t_swap *a, t_swap *b, t_flag *flg)
+void	ft_rrr_ab(t_swap *a, t_swap *b, t_flag *flg)
 {
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 	if (flg->color)
-		flg->verbose ? ft_printf("{y}rr{0}\n") : ft_printf("{y}rr{0}");
+		flg->verbose ? ft_printf("{y}rrr{0}\n") : ft_printf("{y}rrr{0} ");
 	else
-		flg->verbose ? ft_printf("rr\n") : ft_printf("rr");
-	rr(a, b);
+		flg->verbose ? ft_printf("rrr\n") : ft_printf("rrr ");
+	rrr(a, b);
 	flg->verbose ? ft_flag_v(a, b) : 0;
 	flg->cnt += 1;
-	ft_calls_rev_rot(a, b, flg);
+	ft_calls_rot(a, b, flg);
 	ft_is_sorted(a) && ft_is_null(b) ? ft_success(a, b, *flg) : 0;
 }
