6d03fe11b73236e5dcc369adcf70a1307d876362
Tbouder <tbouder@protonmail.com>
2016-04-13 18:01:29 +0200

    Auto-Update 18:01:28, Wednesday 13 April

--- ft_recursiv.c
+++ ft_recursiv.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 11:22:24 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/13 17:29:54 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/13 17:57:41 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,9 @@ void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 	if (ft_z(a) > ft_y(a))
 	{
-		ft_z(a) < A && !BNULL && ft_z(b) <= ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+		ft_z(b) < ft_y(b) && ft_z(b) > B  ? ft_rr_b(a, b, flg) : 0;
+
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_ab(a, b, flg) : 0;
-
-
-
 		ft_z(a) < A && !BNULL && ft_z(b) >= ft_y(b) ? ft_rs_a(a, b, flg) : 0;
 		ft_z(a) > A && ft_order(a) ? ft_rr_a(a, b, flg) : 0;
 		ft_z(a) > A && !ft_order(a) ? ft_rrr_a(a, b, flg) : 0;
@@ -47,7 +45,8 @@ void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 	}
 	if (ft_z(a) < ft_y(a))
 	{
-		ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+		ft_z(b) < ft_y(b) ? ft_rr_b(a, b, flg) : 0; // ++> Meilleur globaux
+		// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 
 		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_ab(a, b, flg) : 0;
 		flg->prev < 2 && ft_z(a) > A ? ft_rrr_a(a, b, flg) : 0;
@@ -55,6 +54,7 @@ void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 		ft_z(a) < A ? ft_rpa_b(a, b, flg) : 0;
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_b(a, b, flg) : 0;
 	}
+
 }
 
 void		ft_calls_rev_rot(t_swap *a, t_swap *b, t_flag *flg)
