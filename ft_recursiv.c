8c851e1142751114d80f125c44d6ab2f4283419c
Tbouder <tbouder@protonmail.com>
2016-04-13 15:30:59 +0200

    Auto-Update 15:30:59, Wednesday 13 April

--- ft_recursiv.c
+++ ft_recursiv.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 11:22:24 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/13 14:57:09 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/13 15:30:58 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,30 @@
 
 void		ft_calls_swap(t_swap *a, t_swap *b, t_flag *flg)
 {
+
 	if (ft_z(a) < ft_y(a))
 	{
-		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
-			&& ft_order(a) ? ft_rr_ab(a, b, flg) : 0;
-		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
-			&& !ft_order(a) ? ft_rrr_ab(a, b, flg) : 0;
+		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? ft_rr_ab(a, b, flg) : 0;
+		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && !ft_order(a) ? ft_rrr_ab(a, b, flg) : 0;
 		ft_z(a) > A && ft_z(b) > A && BNULL ? ft_rpb_a(a, b, flg) : 0;
 		ft_z(a) > A && !ft_order(a) ? ft_rrr_a(a, b, flg) : 0;
 		ft_z(a) > A && ft_order(a) ? ft_rr_a(a, b, flg) : 0;
+
 		ft_z(a) < A ? ft_rpa_b(a, b, flg) : 0;
 	}
-	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
 	ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 }
 
 void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 {
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
 	ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
-	ft_z(b) <= ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+	ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
+
 	if (ft_z(a) > ft_y(a))
 	{
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_ab(a, b, flg) : 0;
@@ -51,12 +56,16 @@ void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 		ft_z(a) < A ? ft_rpa_b(a, b, flg) : 0;
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_b(a, b, flg) : 0;
 	}
-	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
-	ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 }
 
 void		ft_calls_rev_rot(t_swap *a, t_swap *b, t_flag *flg)
 {
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
 	if (ft_z(a) > ft_y(a))
 	{
 		!BNULL && ft_z(b) < ft_y(b) ? ft_rs_ab(a, b, flg) : 0;
@@ -65,25 +74,25 @@ void		ft_calls_rev_rot(t_swap *a, t_swap *b, t_flag *flg)
 	}
 	if (ft_z(a) < ft_y(a))
 	{
-		ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a)
-			|| ft_is_sorted(a->next)) ? ft_rrr_a(a, b, flg) : 0;
-		ft_z(a) < A && ft_y(a) > A ? ft_rpa_b(a, b, flg) : 0;
-		ft_z(a) < A && ft_y(a) < A ? ft_rpa_b(a, b, flg) : 0;
+		ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a) || ft_is_sorted(a->next)) ? ft_rrr_a(a, b, flg) : 0;
+		ft_z(a) < A ? ft_rpa_b(a, b, flg) : 0;
 		ft_z(a) > A && !BNULL && ft_z(b) > A ? ft_rpb_a(a, b, flg) : 0;
-		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
-			&& !ft_order(a) ? ft_rrr_ab(a, b, flg) : 0;
-		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
-			&& ft_order(a) ? ft_rr_ab(a, b, flg) : 0;
+		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && !ft_order(a) ? ft_rrr_ab(a, b, flg) : 0;
+		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? ft_rr_ab(a, b, flg) : 0;
 		ft_z(a) > A && !ft_order(a) ? ft_rrr_a(a, b, flg) : 0;
 		ft_z(a) > A && ft_order(a) ? ft_rr_a(a, b, flg) : 0;
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_b(a, b, flg) : 0;
 	}
-	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
-	ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 }
 
 void		ft_calls_rot(t_swap *a, t_swap *b, t_flag *flg)
 {
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
 	if (ft_z(a) > ft_y(a))
 	{
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_ab(a, b, flg) : 0;
@@ -100,6 +109,7 @@ void		ft_calls_rot(t_swap *a, t_swap *b, t_flag *flg)
 		ft_z(a) > A ? ft_rr_a(a, b, flg) : 0;
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_b(a, b, flg) : 0;
 	}
-	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
-	ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
+
+	// ft_z(b) >= ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
+	// ft_z(b) < ft_y(b) && ft_z(b) > B ? ft_rr_b(a, b, flg) : 0;
 }
