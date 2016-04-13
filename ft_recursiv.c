f14f42240e6a4df74727ef2ea7cf6439965ed30a
Tbouder <tbouder@protonmail.com>
2016-04-13 14:00:47 +0200

    Auto-Update 14:00:47, Wednesday 13 April

--- ft_recursiv.c
+++ ft_recursiv.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 11:22:24 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/12 19:08:52 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/13 13:59:54 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,11 @@ void		ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 	{
 		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_ab(a, b, flg) : 0;
 
-		flg->prev < 2 && ft_z(a) > A ? ft_rrr_a(a, b, flg) : 0;
-		flg->prev == 2 && ft_z(a) > A ? ft_rr_a(a, b, flg) : 0;
+		// flg->prev == 0 && ft_z(a) > A ? ft_rrr_a(a, b, flg) : 0;
+		flg->prev < 2 && ft_z(a) > A ?ft_putnbr(flg->prev), ft_rrr_a(a, b, flg) : 0;
+		// flg->prev == 1 && ft_z(a) > A && ft_y(a) > ft_z(a) ? ft_rr_a(a, b, flg) : 0;
+		flg->prev == 2 && ft_z(a) > A ?ft_putnbr(flg->prev), ft_rr_a(a, b, flg) : 0;
+
 
 
 		ft_z(a) < A ? ft_rpa_b(a, b, flg) : 0;
@@ -69,7 +72,7 @@ void		ft_calls_rev_rot(t_swap *a, t_swap *b, t_flag *flg)
 		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && !ft_order(a) ? ft_rrr_ab(a, b, flg) : 0;
 		ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? ft_rr_ab(a, b, flg) : 0;
 		ft_z(a) > A && !ft_order(a) ? ft_rrr_a(a, b, flg) : 0;
-		ft_z(a) > A && ft_order(a) ? ft_rr_a(a, b, flg) : 0;
+		ft_z(a) > A && ft_order(a) ? ft_rr_a(a, b, flg) : 0; //AKI
 		ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? ft_rs_b(a, b, flg) : 0;
 	}
 	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
@@ -94,3 +97,6 @@ void		ft_calls_rot(t_swap *a, t_swap *b, t_flag *flg)
 	}
 	ft_z(b) > ft_y(b) && ft_z(b) < B ? ft_rrr_b(a, b, flg) : 0;
 }
+
+// RA  ->
+// RRA <-
