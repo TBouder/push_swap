6223bce7bcd9a9999b63c49dacfeefe2c6244664
Tbouder <tbouder@protonmail.com>
2016-04-19 20:14:43 +0200

    Push with final(?) rev

--- ft_algo.c
+++ ft_algo.c
@@ -6,29 +6,30 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 11:22:24 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/19 18:08:57 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/19 20:14:00 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 #define BNULL ft_is_null(b)
 
-int			ft_calls_swap(t_swap *a, t_swap *b, t_flag *flg)
+static int	ft_call_push_helper(t_swap *a, t_swap *b)
 {
 	int		r;
 
 	r = 0;
-	if (ft_z(a) < ft_y(a))
-	{
-		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? r = RR : 0;
-		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && !ft_order(a) ? r = RRR : 0;
-		!r && ft_z(a) > A && flg->prev < 2 ? r = RRA : 0;
-		!r && ft_z(a) > A && flg->prev == 2 ? r = RA : 0;
-		!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a) || ft_is_sorted(a->next)) ? r = RRA : 0;
-		!r && ft_z(a) < A ? r = PB : 0;
-	}
-	!r && ft_z(b) >= ft_y(b) && ft_z(b) < B ? r = RRB : 0;
-	!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B && flg->prev == 2 ? r = RB : 0;
+	!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B ? r = SB : 0;
+	!r && !BNULL && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B
+		? r = RRR : 0;
+	!r && !BNULL && ft_z(b) < ft_y(b) ? r = RB : 0;
+	!r && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a)
+		? r = RR : 0;
+	!r && ft_z(a) < A && a->next->next && a->next->next->stack > a->next->stack
+		? r = PB : 0;
+	!r && ft_z(a) < A && a->next && a->next->stack > A ? r = PB : 0;
+	!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a)
+		|| ft_is_sorted(a->next)) ? r = RRA : 0;
+	!r && ft_z(a) < A ? r = PB : 0;
 	return (r);
 }
 
@@ -40,28 +41,39 @@ int			ft_calls_push(t_swap *a, t_swap *b, t_flag *flg)
 	!r && ft_z(b) >= ft_y(b) && ft_z(b) < B ? r = RRB : 0;
 	!r && ft_z(a) > A && flg->prev < 2 ? r = RRA : 0;
 	!r && ft_z(a) > A && flg->prev == 2 ? r = RA : 0;
-	if (ft_z(a) > ft_y(a))
+	if (!r && ft_z(a) > ft_y(a))
 	{
-		!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B && flg->prev == 2 ? r = RB : 0;
+		!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B && flg->prev == 2
+		? r = RB : 0;
 		!r && !BNULL && ft_z(a) < A && ft_z(b) < ft_y(b) ? r = SS : 0;
 		!r && !BNULL && ft_z(a) < A && ft_z(b) >= ft_y(b) ? r = SA : 0;
 		!r && !BNULL && ft_z(a) > A && ft_z(b) < ft_y(b) ? r = SB : 0;
 	}
-	else if (ft_z(a) < ft_y(a))
+	else if (!r && ft_z(a) < ft_y(a))
+		r = ft_call_push_helper(a, b);
+	return (r);
+}
+
+int			ft_calls_swap(t_swap *a, t_swap *b, t_flag *flg)
+{
+	int		r;
+
+	r = 0;
+	if (ft_z(a) < ft_y(a))
 	{
-		!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B ? r = SB : 0;
-		!r && !BNULL && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B ? r = RRR : 0;
-		!r && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? r = RR : 0;
-
-
-
-
-		!r && ft_z(b) < ft_y(b) ? r = RB : 0;
-		!r && ft_z(a) < A && a->next->next && a->next->next->stack > a->next->stack ? r = PB : 0;
-		!r && ft_z(a) < A && a->next && a->next->stack > A ? r = PB : 0;
-		!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a) || ft_is_sorted(a->next)) ? r = RRA : 0;
+		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
+			&& ft_order(a) ? r = RR : 0;
+		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
+			&& !ft_order(a) ? r = RRR : 0;
+		!r && ft_z(a) > A && flg->prev < 2 ? r = RRA : 0;
+		!r && ft_z(a) > A && flg->prev == 2 ? r = RA : 0;
+		!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a)
+			|| ft_is_sorted(a->next)) ? r = RRA : 0;
 		!r && ft_z(a) < A ? r = PB : 0;
 	}
+	!r && ft_z(b) >= ft_y(b) && ft_z(b) < B ? r = RRB : 0;
+	!r && !BNULL && ft_z(b) < ft_y(b) && ft_z(b) > B && flg->prev == 2
+		? r = RB : 0;
 	return (r);
 }
 
@@ -79,10 +91,12 @@ int			ft_calls_rev_rot(t_swap *a, t_swap *b)
 	}
 	if (ft_z(a) < ft_y(a))
 	{
-		!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a) || ft_is_sorted(a->next)) ? r = RRA : 0;
+		!r && ft_z(a) < A && ft_y(a) > A && (a->next->stack < ft_z(a)
+			|| ft_is_sorted(a->next)) ? r = RRA : 0;
 		!r && ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? r = SB : 0;
 		!r && ft_z(a) < A ? r = PB : 0;
-		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B && !ft_order(a) ? r = RRR : 0;
+		!r && ft_z(a) > A && !BNULL && ft_z(b) > ft_y(b) && ft_z(b) < B
+			&& !ft_order(a) ? r = RRR : 0;
 		!r && ft_z(a) > A && !BNULL && ft_z(b) > A ? r = PA : 0;
 		!r && ft_z(a) > A && !ft_order(a) ? r = RRA : 0;
 	}
@@ -104,9 +118,12 @@ int			ft_calls_rot(t_swap *a, t_swap *b)
 	}
 	if (ft_z(a) < ft_y(a))
 	{
+		!r && ft_z(a) < A && ft_y(a) > A && (ft_w(a) > A
+			|| ft_is_r_sorted(a->end)) ? r = RA : 0;
 		!r && ft_z(a) < A && !BNULL && ft_z(b) < ft_y(b) ? r = SB : 0;
 		!r && ft_z(a) < A ? r = PB : 0;
-		!r && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B && ft_order(a) ? r = RR : 0;
+		!r && ft_z(a) > A && ft_z(b) > ft_y(b) && ft_z(b) < B
+			&& ft_order(a) ? r = RR : 0;
 		!r && ft_z(a) > A && !BNULL && ft_z(b) > A ? r = PA : 0;
 		!r && ft_z(a) > A && ft_order(a) ? r = RA : 0;
 	}
